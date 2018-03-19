e7de8981e14a23a467bcef459ee95a5293622373
Jakub Lankiewicz <lankiewiczjakub@gmail.com>
2018-03-19 21:57:01 +0100

    do weryfiacji w pracy

--- js/script.js
+++ js/script.js
@@ -5,19 +5,18 @@ var countriesList = $('#countries');
 $('#search').click(searchCountries);
 
 function searchCountries() {
-  	var countryName = $('#country-name').val();
-	if(!countryName.length) countryName = 'Poland';
-}
-
-
-function searchCountries() {
  	var countryName = $('#country-name').val();
 if(!countryName.length) countryName = 'Poland';
-
-
 $.ajax({
   		url: url + countryName,
   		method: 'GET',
   		success: showCountriesList
   	});
+}
+
+function showCountriesList(resp) {
+  countriesList.empty();
+  resp.forEach(function(item){
+   	$('<li>').text(item.name).appendTo(countriesList);
+});
 }
